aaf2f17648e2059cd4b4f804917809e9fcd13cbc
Get-it-right <85377605+Get-it-right@users.noreply.github.com>
2021-06-20 16:28:07 +0800

    Add files via upload

--- c.js
+++ c.js
@@ -7,11 +7,9 @@ server.on("request",(req,res)=>{
     if(req.method=="POST"){
         var form = new formidable.IncomingForm();//接收新文件
         form.multiples = true;//带着就是
-        form.uploadDir =path.join(__dirname,"upload");//__dirname全局变量,表示网站根目录。添加到upload文件夹      
-        form.parse(req,function(err,fields,files){ 
-            let bitmap = fs.readFileSync(files.a.path,function (err) {
-                if (err) throw err;
-            });
+        form.uploadDir =path.join(__dirname,"upload");//添加到upload文件夹              
+        form.parse(req,function(err,fields,files){    
+            let bitmap = fs.readFileSync(files.a.path);
             let base64str = Buffer.from(bitmap, 'binary').toString('base64'); // base64编码
             let stringbase64=base64str+"";
             const tencentcloud = require("tencentcloud-sdk-nodejs");
@@ -37,47 +35,15 @@ server.on("request",(req,res)=>{
                     res.writeHead(200, {
                         'content-type': 'text/html;charset=utf8'
                     });
-                    fs.writeFile("a.txt","腾讯云识别结果：",function (err) {
-                        if (err) throw err;
-                    });
-                    i=0;
-                    while(i<data.TextDetections.length){
-                        fs.appendFile("a.txt",data.TextDetections[i].DetectedText,function (err) {
-                            if (err) throw err;
-                    });
-                        i=i+1;
-                    }
+                    res.end(data.TextDetections[0].DetectedText);
                 },
                 (err) => {
                     console.error("error", err);
                 }
             );
-            var AipOcrClient = require("baidu-aip-sdk").ocr;
-            var APP_ID = "24315977";
-            var API_KEY = "fkGSfQ0DPkq3fcDnqOGAfufK";
-            var SECRET_KEY = "wi6qyUzhytDrusdFbTa14s3lhaxIlLEj";
-            var client2 = new AipOcrClient(APP_ID, API_KEY, SECRET_KEY);
-            client2.generalBasic(stringbase64).then(
-                (result) => {
-                fs.writeFile("b.txt","百度云识别结果：",function (err) {
-                        if (err) throw err;
-                    });
-                i=0;
-                while(i<result.words_result.length){
-                    fs.appendFile("b.txt",result.words_result[i].words,function (err) {
-                        if (err) throw err;
-                    });
-                    i=i+1;
-                }
-                res.end(" ");
-                },
-                (err) => {
-                    res.end(err);
-                }
-            );
         });
     }
-});
-server.listen(15833,"10.0.4.2",()=>{
+})
+server.listen(4000,()=>{
     console.log("Server is running...");
-});
+})
